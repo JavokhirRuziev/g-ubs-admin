38f204b28dfdcbe50eabb235056836744e30f201
XasanDeveloper <mirkhasan96@yandex.ru>
2022-05-18 15:43:48 +0500

    dashboard statistics

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import DashboardChart from "./dashboardChart";
+import { useDispatch } from "react-redux";
+import Actions from "modules/entity/actions";
+
+import get from "lodash/get";
 import "./style.scss";
 
 const Index = () => {
+    const dispatch = useDispatch();
+    const [statistic, setStatistic] = useState(null);
+
+    const loadDashboardStat = () => {
+        dispatch(Actions.LoadDefault.request({
+            url: `/dashboard/total`,
+            cb: {
+                success: data => {
+                    setStatistic(data)
+                },
+                error: () => {
+                }
+            }
+        }))
+    }
+
+    useEffect(() => {
+        loadDashboardStat()
+    }, [])
+
     return (
         <div>
             <div className="title-md mb-20">Dashboard</div>
@@ -15,7 +39,7 @@ const Index = () => {
                         <div>
                             <div className="dashboard-card__label">Количество заказов</div>
                             <div className="dashboard-card__num">
-                                <span>182</span>
+                                <span>{get(statistic, 'orders', 0)}</span>
                                 <span>шт</span>
                             </div>
                         </div>
@@ -27,7 +51,7 @@ const Index = () => {
                         <div>
                             <div className="dashboard-card__label">Количество контрагентов</div>
                             <div className="dashboard-card__num">
-                                <span>77</span>
+                                <span>{get(statistic, 'companies', 0)}</span>
                                 <span>шт</span>
                             </div>
                         </div>
@@ -39,7 +63,7 @@ const Index = () => {
                         <div>
                             <div className="dashboard-card__label">Количество пользователей</div>
                             <div className="dashboard-card__num">
-                                <span>99</span>
+                                <span>{get(statistic, 'users', 0)}</span>
                                 <span>шт</span>
                             </div>
                         </div>
@@ -51,7 +75,7 @@ const Index = () => {
                         <div>
                             <div className="dashboard-card__label">Количество эды</div>
                             <div className="dashboard-card__num">
-                                <span>200</span>
+                                <span>{get(statistic, 'dishes', 0)}</span>
                                 <span>шт</span>
                             </div>
                         </div>
