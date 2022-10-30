a0a6d8b76efaca0e6aa2231f491bbd8fe54b725a
xusDev <14219606gitlab>
2022-10-30 18:04:54 +0500

    commit

--- src/pages/Dishes/List.jsx
+++ src/pages/Dishes/List.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 import {Table, Board, Avatar} from "components";
-import {Button, Pagination, Spin, Tabs, Modal, notification} from "antd";
+import {Input, Button, Pagination, Spin, Tabs, Modal, notification} from "antd";
 import EntityContainer from 'modules/entity/containers';
 import Actions from "modules/entity/actions";
 
@@ -61,6 +61,74 @@ const List = ({history, location}) => {
       }
     }))
   };
+
+  const quickUpdate = (id, value) => {
+    dispatch(Actions.Form.request({
+      method: 'put',
+      entity: "dishes",
+      name: `all-${tabLang}`,
+      id: id,
+      url: `/dishes/quick-update/${id}`,
+      primaryKey: 'id',
+      updateData: true,
+      normalizeData: data => data,
+      params: {
+        extra: { _l: tabLang }
+      },
+      values: {
+        quantity: value
+      },
+      cb: {
+        success: () => {
+          notification["success"]({
+            message: t("Успешно обновлено"),
+            duration: 2
+          });
+        },
+        error: () => {
+          notification["error"]({
+            message: t("Что-то пошло не так"),
+            duration: 2
+          });
+        },
+        finally: () => {}
+      }
+    }))
+  }
+
+  const quickUpdateStatus = (id, value) => {
+    dispatch(Actions.Form.request({
+      method: 'put',
+      entity: "dishes",
+      name: `all-${tabLang}`,
+      id: id,
+      url: `/dishes/quick-update/${id}`,
+      primaryKey: 'id',
+      updateData: true,
+      normalizeData: data => data,
+      params: {
+        extra: { _l: tabLang }
+      },
+      values: {
+        status: value === 1 ? 0 : 1
+      },
+      cb: {
+        success: () => {
+          notification["success"]({
+            message: t("Успешно обновлено"),
+            duration: 2
+          });
+        },
+        error: () => {
+          notification["error"]({
+            message: t("Что-то пошло не так"),
+            duration: 2
+          });
+        },
+        finally: () => {}
+      }
+    }))
+  }
 
   const TabPane = Tabs.TabPane;
   return (
@@ -145,15 +213,17 @@ const List = ({history, location}) => {
                       {
                         title: t("Кол-во"),
                         dataIndex: "quantity",
-                        render: value => <div className="divider-wrapper">{value ? value : 0}</div>
+                        render: (value, values) => <div>
+                          <Input placeholder={t("Количество")} style={{ width: '120px' }} type={'number'} defaultValue={value} onPressEnter={(e)=>quickUpdate(values.id, e.target.value)} />
+                        </div>
                       },
                       {
                         title: t("Статус"),
                         dataIndex: "status",
                         className: 'text-cen w-82',
-                        render: value => {
+                        render: (value, values) => {
                           return <div className="divider-wrapper">
-                            <div className="color-view-ellipse m-0-auto" style={{backgroundColor: value === 1 ? '#4caf50' : '#f44336'}}/>
+                            <div className="color-view-ellipse m-0-auto cursor-pointer" onClick={() => quickUpdateStatus(values.id, value)} style={{backgroundColor: value === 1 ? '#4caf50' : '#f44336'}}/>
                           </div>
                         }
                       }
